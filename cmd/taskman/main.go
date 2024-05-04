2fd49e51539f7151e4bd8c1492c16ee4428c93f9
Zach Ross <zross@Zachs-MBP.lan>
2024-05-03 21:20:23 -0400

    adding fun ascii title

--- cmd/taskman/main.go
+++ cmd/taskman/main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
-	"github.com/zrross11/golang-task-manager/cmd/taskman/cmd"
+	"fmt"
 	"os"
+
+	"github.com/gookit/color"
+	"github.com/zrross11/golang-task-manager/cmd/taskman/cmd"
 )
 
 func main() {
+	color.Style{color.Green, color.OpBold}.Println(`
+________                    __                                         
+|        \                  |  \                                        
+ \$$$$$$$$______    _______ | $$   __  ______ ____    ______   _______  
+   | $$  |      \  /       \| $$  /  \|      \    \  |      \ |       \ 
+   | $$   \$$$$$$\|  $$$$$$$| $$_/  $$| $$$$$$\$$$$\  \$$$$$$\| $$$$$$$\
+   | $$  /      $$ \$$    \ | $$   $$ | $$ | $$ | $$ /      $$| $$  | $$
+   | $$ |  $$$$$$$ _\$$$$$$\| $$$$$$\ | $$ | $$ | $$|  $$$$$$$| $$  | $$
+   | $$  \$$    $$|       $$| $$  \$$\| $$ | $$ | $$ \$$    $$| $$  | $$
+    \$$   \$$$$$$$ \$$$$$$$  \$$   \$$ \$$  \$$  \$$  \$$$$$$$ \$$   \$$`)
+	fmt.Println()
+
 	rootCmd := cmd.NewRootCmd()
 
 	if err := cmd.Execute(rootCmd); err != nil {
