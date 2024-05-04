b65f209a13756052d754c6270e2006b17c52225e
Zach Ross <zross@Zachs-MBP.lan>
2024-05-03 20:42:33 -0400

    playing around with arg functionality

--- cmd/taskman/cmd/add.go
+++ cmd/taskman/cmd/add.go
@@ -1,16 +1,33 @@
 package cmd
 
 import (
-    "github.com/spf13/cobra"
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
 	"github.com/zrross11/golang-task-manager/pkg/task"
 )
 
 var AddCmd = &cobra.Command{
-    Use:   "add",
-    Aliases: []string{"a"},
-    Short:  "Add a task to the manager",
-    Args:  cobra.ExactArgs(1),
-    Run: func(cmd *cobra.Command, args []string) {
-		task.AddTask(args[0])
-    },
+	Use:     "add",
+	Aliases: []string{"a"},
+	Short:   "Add a task to the manager",
+	Args:    cobra.MinimumNArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fmt.Println("Please provide a task to add:")
+			reader := bufio.NewReader(os.Stdin)
+			taskName, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("Error reading user input")
+				return
+			}
+			taskName = taskName[:len(taskName)-1]
+			task.AddTask(taskName)
+			return
+		} else {
+			task.AddTask(args[0])
+		}
+	},
 }
--- cmd/taskman/cmd/remove.go
+++ cmd/taskman/cmd/remove.go
@@ -1,16 +1,33 @@
 package cmd
 
 import (
-    "github.com/spf13/cobra"
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
 	"github.com/zrross11/golang-task-manager/pkg/task"
 )
 
 var RemoveCmd = &cobra.Command{
-    Use:   "remove",
-    Aliases: []string{"r"},
-    Short:  "Remove a task from the manager",
-    Args:  cobra.ExactArgs(1),
-    Run: func(cmd *cobra.Command, args []string) {
-		task.RemoveTask(args[0])
-    },
+	Use:     "remove",
+	Aliases: []string{"r"},
+	Short:   "Remove a task from the manager",
+	Args:    cobra.MinimumNArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fmt.Println("Please provide a task to remove:")
+			reader := bufio.NewReader(os.Stdin)
+			taskName, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("Error reading user input")
+				return
+			}
+			taskName = taskName[:len(taskName)-1]
+			task.RemoveTask(taskName)
+			return
+		} else {
+			task.RemoveTask(args[0])
+		}
+	},
 }
